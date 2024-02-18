8685c817d1877a52be5174fdde08cfb21af33110
Vinicius Gregorio <vincamgreg@hotmail.com>
2024-02-18 20:56:11 -0300

    feat: get kafka messages from balances topic

--- balance-app/go.mod
+++ balance-app/go.mod
@@ -3,12 +3,16 @@ module github.com/vinicius-gregorio/balance-api
 go 1.21.1
 
 require (
-	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0
 	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- balance-app/main.go
+++ balance-app/main.go
@@ -2,16 +2,52 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func main() {
-
 	fmt.Println("Hello, World!")
 
-	// configMap := ckafka.ConfigMap{
-	// 	"bootstrap.servers": "kafka:29092",
-	// 	"group.id":          "wallet",
-	// }
-	// kafkaConsumer := kafka.NewConsumer(&configMap, []string{"balance"})
-	// kafkaConsumer.ConfigMap = &configMap
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "kafka:29092",
+		"group.id":          "wallet",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		panic(fmt.Errorf("failed to create consumer: %v", err))
+	}
+	defer c.Close()
+
+	for {
+		err := subscribeToBalancesTopic(c)
+		if err != nil {
+			fmt.Printf("Failed to subscribe to balances topic: %v\n", err)
+			time.Sleep(5 * time.Second) // Retry after 5 seconds
+		} else {
+			break // Exit the loop if successfully subscribed
+		}
+	}
+
+	run := true
+	for run {
+		msg, err := c.ReadMessage(time.Second)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else if !isTimeoutError(err) {
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+}
+
+func subscribeToBalancesTopic(c *kafka.Consumer) error {
+	topics := []string{"balances"}
+	err := c.SubscribeTopics(topics, nil)
+	return err
+}
+
+func isTimeoutError(err error) bool {
+	kafkaErr, ok := err.(kafka.Error)
+	return ok && kafkaErr.IsTimeout()
 }
